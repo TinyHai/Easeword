e872f6ac605977d53a3f7a277b8526bf2bfa902d
TinyHai <1035908357@qq.com>
2020-04-06 20:37:07 +0800

    添加说文解字字体包

--- app/src/main/java/cn/tyhyh/easeword/util/FontUtil.kt
+++ app/src/main/java/cn/tyhyh/easeword/util/FontUtil.kt
@@ -2,25 +2,38 @@ package cn.tyhyh.easeword.util
 
 import android.graphics.Typeface
 import android.widget.TextView
+import cn.tyhyh.easeword.EaseWordApplication
 
 object FontUtil {
 
-    const val YRDZS_PATH = "fonts/yrdzs.ttf"
+    const val YRDZS = "fonts/yrdzs.ttf"
+
+    const val SWJZ = "fonts/swjz.ttf"
+
+
+    private val fontPaths = arrayOf(
+        SWJZ,
+        YRDZS
+    )
 
     private val fontCache = HashMap<String, Typeface>()
 
     fun setTypefaceForTextView(tv: TextView, fontPath: String) {
         tv.typeface = getTypefaceFromCache(fontPath) ?: {
-            val context = tv.context
-            val typeface = Typeface.createFromAsset(context.assets, fontPath)
-            if (typeface != null) {
-                fontCache[fontPath] = typeface
-            }
-            typeface
+            initTypeface()
+            fontCache[fontPath]
         }()
     }
 
     private fun getTypefaceFromCache(fontPath: String): Typeface? {
         return fontCache[fontPath]
+    }
+
+    private fun initTypeface() {
+        val application = EaseWordApplication.getApplication()
+        fontPaths.forEach {
+            val typeface = Typeface.createFromAsset(application.assets, it)
+            fontCache[it] = typeface
+        }
     }
 }
