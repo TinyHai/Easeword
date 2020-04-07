84aedaa6af8f14886c9ca75c3a81e672dc05a9d8
TinyHai <1035908357@qq.com>
2020-04-07 13:39:34 +0800

    优化细节

--- app/src/main/java/cn/tyhyh/easeword/EaseWordApplication.kt
+++ app/src/main/java/cn/tyhyh/easeword/EaseWordApplication.kt
@@ -1,6 +1,10 @@
 package cn.tyhyh.easeword
 
 import android.app.Application
+import cn.tyhyh.easeword.data.db.EaseWordDB
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
 import org.litepal.LitePal
 
 class EaseWordApplication : Application() {
@@ -9,6 +13,9 @@ class EaseWordApplication : Application() {
         super.onCreate()
         instance = this
         LitePal.initialize(this)
+        CoroutineScope(Dispatchers.IO).launch {
+            EaseWordDB.initFromAsset()
+        }
     }
 
     companion object {
